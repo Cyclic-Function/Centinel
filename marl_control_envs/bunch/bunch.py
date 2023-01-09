df0efa0fe4182f09f6d609d3b645a879c9384835
Yash <ysg22@cam.ac.uk>
2023-01-09 20:00:38 +0000

    maxwelton brae

--- marl_control_envs/bunch/bunch.py
+++ marl_control_envs/bunch/bunch.py
@@ -163,7 +163,7 @@ class Scenario(BaseScenario):
         if agent.goal_b is not None:
             goal_color[1] = agent.goal_b.color
         
-        print('colour*', agent.goal_b.color)
+        print('agent: ', agent, ' colour:', agent.goal_b.color)
 
         # get positions of all entities in this agent's reference frame
         entity_pos = []
